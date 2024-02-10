292a8b017e0f27c3f8513fc0831368d1da0b92f9
Mahdi Rezaei <mahdir798@gmail.com>
2024-02-10 23:23:31 +0330

    improve theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div>Dashbosrd</div>;
+};
+
+export default Dashboard;
