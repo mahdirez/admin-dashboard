4c944339ec4cbc347964c75135e93499fbd62fd1
Mahdi Rezaei <mahdir798@gmail.com>
2024-03-18 23:45:40 +0330

    create calender

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { useState } from "react";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calender";
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
@@ -26,6 +27,7 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
